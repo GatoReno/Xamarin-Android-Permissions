79f3f0de9f78341e654c8cd1e8aa79cbb9f4e407
Gatoreno <erp.reno@hotmail.com>
2020-11-17 23:19:40 -0500

    adding location permission

---
+++ Permisions.Droid/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Essentials;
+
+namespace Permisions.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        
+
+        private async void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+            Snackbar.Make(view, "ASking permissions", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+
+
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (status != PermissionStatus.Granted)
+            {
+               var resp = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+                if (resp == PermissionStatus.Granted)
+                {
+                    Toast.MakeText(this, "Location granted", ToastLength.Long).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Location denied", ToastLength.Long).Show();
+                }
+            }
+            else {
+                Toast.MakeText(this, "Location granted", ToastLength.Long).Show();
+            }
+           
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
